68cd21d3cfd56530e0cbc62b2af366a92242053f
rajat004 <rajatyadavvns101@gmail.com>
2017-03-25 01:43:31 +0530

    handling edge cases

--- task/src/main/java/core/ClientNumber.java
+++ task/src/main/java/core/ClientNumber.java
@@ -9,6 +9,8 @@ import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
+import constants.ClientNumberConstants;
+
 
 @Entity
 @Table(name = "number")
@@ -18,13 +20,18 @@ import javax.persistence.Table;
     @NamedQuery(name="core.ClientNumber.findNextAvailNumber",
     		query="select cn from ClientNumber cn "
     			+ "where cn.active=0"),
-    @NamedQuery(name="core.ClientNumber.createNewInactiveNumbersInDb",
-	query="select cn from ClientNumber cn "
-		+ "where cn.active=0")
     /*@NamedQuery(name="core.ClientNumber.createNewInactiveNumbersInDb",
+	query="select cn from ClientNumber cn "
+		+ "where cn.active=0"),
+   /* @NamedQuery(name="core.ClientNumber.createNewInactiveNumbersInDb",
     		query="insert into ClientNumber (id) "
     				+ "Select (cn.id + 1) as cn.id  from ClientNumber cn where not exists"
     				+ "(select cn2 from ClientNumber cn2 where cn2.id = cn1.id +1)")*/
+    @NamedQuery(name="core.ClientNumber.createNewInactiveNumbersInDb",
+    		query="select l.id + 1 as id "
+    				+ "from ClientNumber  l left outer join ClientNumber "
+    				+ " r on l.id + 1 = r.id where r.id is null")
+
 })
 public class ClientNumber implements Serializable{
 	
@@ -33,6 +40,15 @@ public class ClientNumber implements Serializable{
 	
 	@Column
 	private int active;
+
+	public ClientNumber(Long id) {
+		this.id=id;
+		this.active=ClientNumberConstants.IN_ACTIVE;
+	}
+	
+	public ClientNumber(){
+		
+	}
 
 	public int getActive() {
 		return active;
--- task/src/main/java/db/ClientNumberDao.java
+++ task/src/main/java/db/ClientNumberDao.java
@@ -1,16 +1,20 @@
 package db;
 
+import java.util.List;
+
+import javax.transaction.Transaction;
 import javax.ws.rs.BadRequestException;
 import javax.ws.rs.WebApplicationException;
 
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
-import com.google.common.base.Optional;
-
-import antlr.collections.List;
 import constants.ClientNumberConstants;
 import core.ClientNumber;
 import io.dropwizard.hibernate.AbstractDAO;
+import static java.util.Objects.requireNonNull;
+
 
 @SuppressWarnings("unused")
 public class ClientNumberDao extends AbstractDAO<ClientNumber> {
@@ -27,7 +31,7 @@ public class ClientNumberDao extends AbstractDAO<ClientNumber> {
 	public ClientNumber findNextAvailNumber() {
 		java.util.List<ClientNumber> listClientNumber = list(
 				namedQuery("core.ClientNumber.findNextAvailNumber").setMaxResults(1));
-		return listClientNumber.get(0);
+		return listClientNumber.isEmpty() ? null : listClientNumber.get(0);
 	}
 
 	public ClientNumber updateStatusOfNumber(ClientNumber clientNumber) {
@@ -47,8 +51,12 @@ public class ClientNumberDao extends AbstractDAO<ClientNumber> {
 	public ClientNumber createAndGetNextAvailNumber() {
 		ClientNumber clientNumber = this.findNextAvailNumber();
 		if (clientNumber == null) {
+			System.out.println("invoking create native query");
 			this.createNewInactiveNumbersInDb();
+			System.out.println("invoking find next avail num  query");
 			clientNumber = this.findNextAvailNumber();
+			if (clientNumber == null)
+				throw new WebApplicationException("could not create new numbers");
 		}
 		if (clientNumber.getId() > ClientNumberConstants.MAX_DEFINED_NUMBER) {
 			throw new BadRequestException("Sorry , any number is not available");
@@ -63,9 +71,33 @@ public class ClientNumberDao extends AbstractDAO<ClientNumber> {
 	}
 
 	private void createNewInactiveNumbersInDb() {
-		namedQuery("core.ClientNumber.createNewInactiveNumbersInDb")
+		Query q = namedQuery("core.ClientNumber.createNewInactiveNumbersInDb")
 				.setMaxResults(ClientNumberConstants.MAX_NEW_NUMBERS_CREATION_LIMIT);
 
+		Session session = currentSession();
+		org.hibernate.Transaction tx = session.beginTransaction();
+		int count = 0;
+		List<Long> ids = requireNonNull(q).list();
+		
+		//creating InactiveClientNumbers in batches of 
+		//20 each time if any Inactive Number is 
+		//unavailable in the system
+		
+		
+		for (Long id : ids) {
+			ClientNumber clientNumber = new ClientNumber(id);
+			if (id < ClientNumberConstants.MAX_DEFINED_NUMBER) {
+				session.save(clientNumber);
+			}
+			if (count % 20 == 0) {
+				session.flush();
+				session.clear();
+			}
+			count++;
+		}
+
+		tx.commit();
+		//session.close();
 	}
 
 }
